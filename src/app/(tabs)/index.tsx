02ad49c602e404259a33482929808e4cd176866b
Nauris Tisancins <nauristisanchins@gmail.com>
2023-10-07 14:48:08 +0100

    get the list of tracks from api

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,10 +1,52 @@
-import { StyleSheet, FlatList } from 'react-native';
+import { StyleSheet, FlatList, ActivityIndicator } from 'react-native';
 import EditScreenInfo from '../../components/EditScreenInfo';
 import { Text, View } from '../../components/Themed';
-import { tracks } from '../../../assets/data/tracks';
+
 import TrackListItem from '../../components/TrackListItem';
+import { gql, useQuery } from '@apollo/client';
+
+const query = gql`
+  query MyQuery($genres: String!) {
+    recommendations(seed_genres: $genres) {
+      tracks {
+        id
+        name
+        preview_url
+        artists {
+          id
+          name
+        }
+        album {
+          id
+          name
+          images {
+            height
+            url
+            width
+          }
+        }
+      }
+    }
+  }
+`;
 
 export default function HomeScreen() {
+  const { data, loading, error } = useQuery(query, {
+    variables: { genres: 'drum-and-bass,house' },
+  });
+
+  if (loading) {
+    return <ActivityIndicator />;
+  }
+
+  if (error) {
+    return (
+      <Text style={{ color: 'white' }}>Failed to fentch recommendations</Text>
+    );
+  }
+
+  const tracks = data.recommendations?.tracks || [];
+
   return (
     <View>
       <FlatList
